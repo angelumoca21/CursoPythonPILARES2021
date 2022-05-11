c277930601415bf39636de017d8486df43927ef1
angelumoca21 <angelmoca21@gmail.com>
2022-05-10 21:22:37 -0500

    S3

---
+++ s3.py
@@ -0,0 +1,31 @@
+#print(" \"Hola\" ")
+#print("HOLA\nhola\nHola")
+
+#print("HOLA")
+#print("hola")
+#print("Hola")
+
+#print("\\")
+
+#print("DC-6 by Dave Goodman")
+#print("       __|__")
+#print("--@--@--(_)--@--@--")
+
+#Definir una variable
+
+Edad = 25
+radio = 4.2
+nombre = 'Angel'
+status = True
+#print(type(Edad))
+#print(type(radio))
+#print(type(nombre))
+#print(type(status))
+
+#print("Mi nombre es " + nombre + ", tengo " + str(Edad) + " años.")
+#print("El radio del circulo es " + str(radio) + " cm.")
+#print("Y el estado del ventilador esta " + str(status))
+nombre = input("Escribe tu nombre: ")
+edad = input("Escribe tu edad: ")
+edad = int(edad)
+print("Mi nombre es " + nombre + ", tengo " + str(edad) + " años.")
