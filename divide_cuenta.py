4df9136659f08b364ced10063339e37d5e385da0
angelumoca21 <82060334+angelumoca21@users.noreply.github.com>
2022-05-24 15:05:15 -0500

    Create divide_cuenta.py
    Reto a operadores

---
+++ divide_cuenta.py
@@ -0,0 +1,20 @@
+#ejercicio1
+
+"""
+totalCuenta = int(input("Ingresa el total de la cuenta: "))
+personas = int(input("Ingresa la cantidad de personas que comieron: "))
+propina = int(input("Ingresa la cantidad de propina a dar: "))
+
+pagoxpersona = (totalCuenta+propina)/ personas
+
+print("A cada persona le toca pagar $" + str(pagoxpersona))
+"""
+#ejercicio2
+
+totalCuenta = int(input("Ingresa el total de la cuenta: "))
+personas = int(input("Ingresa la cantidad de personas que comieron: "))
+propina = int(input("Ingresa la cantidad de propina a dar: "))
+totalConImpuesto = totalCuenta*1.16
+pagoxpersona = (totalConImpuesto+propina)/personas
+
+print("A cada persona le toca pagar $" + str(pagoxpersona))
