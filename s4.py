c179994c86392763d11b6c492bb464c37384bfe0
angelumoca21 <82060334+angelumoca21@users.noreply.github.com>
2022-05-24 13:02:00 -0500

    Create s4.py
    Código de sesión 4

---
+++ s4.py
@@ -0,0 +1,37 @@
+#num1 = 2
+#num2 = 2
+
+#resultado = 0
+
+#resultado = num1 ** num2
+#print(num1)
+#print(num2)
+#print(resultado)
+
+#resultado = 3*5/(5+3*2)-3**2
+#print(resultado)
+
+#string1 = "Hola"
+#string2 = "a todos"
+
+#resultado = string1 * 2
+#print(resultado)
+
+#var1 = 'A'
+#var2 = 'a'
+
+#resultado = var1 != var2
+
+#print(resultado)
+
+edo1 = True
+edo2 = False
+
+#resultado = not(edo1)
+#print(resultado)
+#resultado = not(edo2)
+#print(resultado)
+
+resultado = edo1 or edo2
+
+print(resultado)
