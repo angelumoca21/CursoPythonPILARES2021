a5854f0b5604981c828ef377ce044e32527141df
angelumoca21 <angelmoca21@gmail.com>
2022-05-31 16:10:16 -0500

    Create s7.py

---
+++ s7.py
@@ -0,0 +1,10 @@
+"""for i in range(0,11,3):
+    print(i)"""
+#Pedir un numero al usuario y con el ciclo for mostrar las tablas de multiplicar del numero ingresado. 
+"""num = int(input("Ingresa el numero que quieres ver sus tablas multiplicar: "))
+for i in range(1,11):
+    resultado = num * i
+    print(str(num) + "x" + str(i) + "=" + str(resultado))"""
+
+for i in range(1,11):
+    print(i,end="--")
