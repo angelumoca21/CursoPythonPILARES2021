9726c8f775184c3d2d1d2ad3e5e8e121feec1fb0
angelumoca21 <angelmoca21@gmail.com>
2022-06-07 14:54:34 -0500

    Create retoCiclos.py

---
+++ retoCiclos.py
@@ -0,0 +1,13 @@
+num = int(input("Ingresa un numero entero positivo:"))
+# for i in range(num,0,-1):
+#     print(i,end=",")
+# print("0")
+
+num = int(input("Ingresa un numero entero positivo:"))
+# while num>=0:
+#     print(num)
+#     num = num - 1
+while not(num<1):
+    print(num,end=",")
+    num = num - 1
+print("0")
